223d26f31072bcdc2586cffa73be45f71304fb74
MattAcefxx <mjbeck1990@gmail.com>
2017-12-20 14:16:30 +1100

    Formatting helper class added

---
+++ Fetcher/Fetcher/StringHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fetcher
+{
+    class StringHelper
+    {
+        public static string GetDatNow()
+        {
+            /* Return current date time */
+            DateTime dateTime = DateTime.Now;
+            return dateTime.ToString() + ": ";
+        }
+    }
+}
