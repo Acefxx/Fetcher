fd7341b7dc772fc7a8aa53b4fde75d62e826d8f4
MattAcefxx <mjbeck1990@gmail.com>
2017-12-20 14:46:40 +1100

    Added console command processing class

--- Fetcher/Fetcher/frmFetcher.cs
+++ Fetcher/Fetcher/frmFetcher.cs
@@ -26,6 +26,14 @@ namespace Fetcher
         {
             /* Posts console commands for processing */
             lstConsole.Items.Add(StringHelper.GetDatNow() + txtInput.Text);
+            if (txtInput.Text != string.Empty)
+            {
+                string response = ConsoleProcess.checkCommand(txtInput.Text);
+                if(response != string.Empty)
+                {
+                    lstConsole.Items.Add(response);
+                }
+            }
             txtInput.Text = string.Empty;
         }
     }
